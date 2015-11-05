37bcd6d3bae8e3bfd39836307ce984e710459fba
uppinder <uppinderchugh@gmail.com>
2015-11-06 03:10:52 +0530

    Update index.php

--- index.php
+++ index.php
@@ -109,7 +109,7 @@ $(window).resize(function(){window.location.href=window.location.href})
             </div>
             <div class="form-group">
               <label for="psw"><span class="glyphicon glyphicon-eye-open"></span> Password</label>
-              <input type="text" class="form-control" id="psw" placeholder="Enter password">
+              <input type="password" class="form-control" id="psw" placeholder="Enter password">
             </div>
             <div class="checkbox">
               <label><input type="checkbox" value="" checked>Remember me</label>
