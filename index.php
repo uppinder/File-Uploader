24fc82c7070aa8edc274f40ef31b36c5bef51f92
uppinder <uppinderchugh@gmail.com>
2015-11-06 02:56:23 +0530

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
   <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-  <<script type="text/javascript">
+  <script type="text/javascript">
   $(window).load(function(){
 $('img.bgfade').hide();
 var dg_H = $(window).height();
